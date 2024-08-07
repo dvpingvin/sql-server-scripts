b1ba433b487e1fab9c8232357d2325897d4a2431
dvpingvin <dvpingvin@gmail.com>
2024-08-07 15:31:35 +1000

    Update security_sensetive_config_params.sql
    Add 'default trace enabled'

--- security_sensetive_config_params.sql
+++ security_sensetive_config_params.sql
@@ -7,23 +7,11 @@ WHERE
 'clr strict security',
 'cross db ownership chaining',
 'Database Mail XPs',
+'default trace enabled',
 'Ole Automation Procedures',
 'remote access',
 'remote admin connections',
 'scan for startup procs',
 'xp_cmdshell'
-)
-OR 
-[configuration_id] IN (
-16391,
-1562,
-1587,
-400,
-16386,
-16388,
-117,
-1576,
-1547,
-16390
 )
 ORDER BY [name]
