2c3c94d44da94843a28a69a9b3807f4dda607aa3
dvpingvin <dvpingvin@gmail.com>
2024-08-07 13:32:50 +1000

    Update regread.sql
    Added more params

--- regread.sql
+++ regread.sql
@@ -2,6 +2,11 @@ USE [master]
 GO
 DECLARE @HideInstance INT
 DECLARE @NamedPipesEnabled INT
+DECLARE @PipeName INT
+DECLARE @SharedMemoryEnabled INT
+DECLARE @TCPEnabled INT
+DECLARE @TcpPort INT
+DECLARE @TcpDynamicPorts INT
 DECLARE @NumErrorLogs INT
 DECLARE @IsErrorReportingEnabled INT
 DECLARE @ErrorLogSizeInKb INT
@@ -14,6 +19,26 @@ EXECUTE master.sys.xp_instance_regread
     'HKEY_LOCAL_MACHINE',
     'Software\Microsoft\MSSQLServer\MSSQLServer\SuperSocketNetLib\Np\',
     'Enabled', @NamedPipesEnabled OUTPUT
+EXECUTE master.sys.xp_instance_regread
+    'HKEY_LOCAL_MACHINE',
+    'Software\Microsoft\MSSQLServer\MSSQLServer\SuperSocketNetLib\Np\',
+    'PipeName', @PipeName OUTPUT
+EXECUTE master.sys.xp_instance_regread
+    'HKEY_LOCAL_MACHINE',
+    'Software\Microsoft\MSSQLServer\MSSQLServer\SuperSocketNetLib\Sm\',
+    'Enabled', @SharedMemoryEnabled OUTPUT
+EXECUTE master.sys.xp_instance_regread
+    'HKEY_LOCAL_MACHINE',
+    'Software\Microsoft\MSSQLServer\MSSQLServer\SuperSocketNetLib\Tcp\',
+    'Enabled', @TCPEnabled OUTPUT
+EXECUTE master.sys.xp_instance_regread
+    'HKEY_LOCAL_MACHINE',
+    'Software\Microsoft\MSSQLServer\MSSQLServer\SuperSocketNetLib\Tcp\IPAll\',
+    'TcpPort', @TcpPort OUTPUT
+EXECUTE master.sys.xp_instance_regread
+    'HKEY_LOCAL_MACHINE',
+    'Software\Microsoft\MSSQLServer\MSSQLServer\SuperSocketNetLib\Tcp\IPAll\',
+    'TcpDynamicPorts', @TcpDynamicPorts OUTPUT
 EXECUTE master.sys.xp_instance_regread
     'HKEY_LOCAL_MACHINE',
     'Software\Microsoft\MSSQLServer\CPE\',
@@ -31,8 +56,13 @@ SELECT
 	SERVERPROPERTY('SERVERNAME') AS [ServerName], 
 	@HideInstance AS [HideInstance],
 	@NamedPipesEnabled AS [NamedPipesEnabled],
-	@IsErrorReportingEnabled AS [IsErrorReportingEnabled], 
-	@NumErrorLogs AS [NumErrorLogs], -- NULL means never changed and has default value (= 6), 0 means was changed to default value (= 6)
-	@ErrorLogSizeInKb AS [ErrorLogSizeInKb], -- NULL means never changed and has default value ('Unlimited'), 0 means was changed to default value ('Unlimited')
+	@PipeName AS [PipeName],
+	@SharedMemoryEnabled AS [SharedMemoryEnabled],
+	@TCPEnabled AS [TCPEnabled],
+	@TcpPort AS [TcpPort],
+	@TcpDynamicPorts AS [TcpDynamicPorts],
+	@IsErrorReportingEnabled AS [IsErrorReportingEnabled],
+	CASE WHEN @NumErrorLogs IS NULL THEN 6 ELSE @NumErrorLogs END AS [NumErrorLogs], 
+	CASE WHEN @ErrorLogSizeInKb IS NULL THEN 'Unlimited' ELSE CAST (@ErrorLogSizeInKb AS VARCHAR(6)) END AS [ErrorLogSizeInKb],
 	SERVERPROPERTY('ErrorLogFileName') AS 'Error log file location'
 GO
