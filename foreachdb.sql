a85073cc5f8ee8c93453d61f45333cbe4a22677f
dvpingvin <dvpingvin@gmail.com>
2024-08-07 10:45:37 +1000

    Update foreachdb.sql
    Chande [result] type size to NVARCHAR(4000)

--- foreachdb.sql
+++ foreachdb.sql
@@ -5,7 +5,7 @@ DECLARE @db_name SYSNAME
 DECLARE @query NVARCHAR(MAX)
 
 DROP TABLE IF EXISTS #t
-CREATE TABLE #t ([db_name] SYSNAME, [result] NVARCHAR(MAX) )
+CREATE TABLE #t ([db_name] SYSNAME, [result] NVARCHAR(4000) )
 INSERT INTO #t ([db_name]) SELECT [name] FROM sys.databases WHERE database_id > 4;
 
 WHILE EXISTS(SELECT [result] FROM #t WHERE [result] IS NULL)
@@ -22,7 +22,7 @@ BEGIN
 		UPDATE #t SET [result] ='OK' WHERE [db_name] = @db_name
 	END TRY
 	BEGIN CATCH
-		UPDATE #t SET [result] =ERROR_MESSAGE() WHERE [db_name] = @db_name
+		UPDATE #t SET [result] = ERROR_MESSAGE() WHERE [db_name] = @db_name
 	END CATCH
 END
 
