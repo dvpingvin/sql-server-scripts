bc9bd829d69edaf5f9a71fcfeebf243be6feea91
dvpingvin <dvpingvin@gmail.com>
2024-08-23 14:51:12 +1000

    Update change_pwd.sql

--- change_pwd.sql
+++ change_pwd.sql
@@ -23,71 +23,66 @@ SELECT @prom_name = [name] FROM master.sys.server_principals WHERE UPPER (name) 
 SELECT @test_name = [name] FROM master.sys.server_principals WHERE UPPER (name) = 'SQLDBATEST'
 SELECT @is_sa_enabled = [is_disabled] FROM master.sys.server_principals WHERE LOWER (name) = 'sa'
 
-IF @prom_name IS NULL 
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), 'SQLDBA', NULL, NULL, 'CHANDGE PWD [SQLDBA]', 'SKIPPED: Login ''SQLDBA'' does not exist'
-ELSE
-BEGIN TRY
-	EXEC ('ALTER LOGIN ' + @prom_name + ' WITH PASSWORD = 0x<hash> HASHED, NAME = [SQLDBA], DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF')
-	GRANT CONNECT SQL TO [SQLDBA]
-	ALTER LOGIN [SQLDBA] ENABLE
-	ALTER SERVER ROLE [sysadmin] ADD MEMBER [SQLDBA]
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), [name], [create_date], [modify_date], 'CHANDGE PWD [SQLDBA]', 'SUCCESS: ' + ERROR_MESSAGE() AS ErrorMessage
-	FROM master.sys.server_principals
-	WHERE UPPER (name) =  UPPER (@prom_name); 
-END TRY  
+INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, login_name, action, ErrorMessage)
+VALUES 
+(GETDATE (), 'SQLDBA', 'CHANDGE PWD [SQLDBA]', 'INITIATION'),
+(GETDATE (), 'SQLDBATest', 'CHANDGE PWD [SQLDBATest]', 'INITIATION'),
+(GETDATE (), 'sa', 'DISABLE [sa]', 'INITIATION')
 
-BEGIN CATCH  
-	IF @prom_name IS NULL SET @prom_name = 'SQLDBA'
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), @prom_name, [create_date], [modify_date], 'CHANDGE PWD [SQLDBA]', 'ERROR: ' + ERROR_MESSAGE() AS ErrorMessage
-	FROM master.sys.server_principals
-	WHERE UPPER (name) =  UPPER (@prom_name); 
-END CATCH;
+IF @prom_name IS NULL
+       UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'SKIPPED: Login ''SQLDBA'' does not exist' WHERE UPPER (login_name) = 'SQLDBA';
+ELSE
+       BEGIN TRY
+             EXEC ('ALTER LOGIN ' + @prom_name + ' WITH PASSWORD = 0x<hash> HASHED, NAME = [SQLDBA], DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF')
+             GRANT CONNECT SQL TO [SQLDBA]
+             ALTER LOGIN [SQLDBA] ENABLE
+             ALTER SERVER ROLE [sysadmin] ADD MEMBER [SQLDBA]
+             UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'SUCCESS: ' + ERROR_MESSAGE()     WHERE UPPER (login_name) =  UPPER (@prom_name); 
+       END TRY  
+
+       BEGIN CATCH
+             UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'ERROR: ' + ERROR_MESSAGE() WHERE UPPER (login_name) =  UPPER (@prom_name); 
+       END CATCH;
 
 IF @test_name IS NULL 
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), 'SQLDBATEST', NULL, NULL, 'CHANDGE PWD [SQLDBATest]', 'SKIPPED: Login ''SQLDBATEST'' does not exist'
+       UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'SKIPPED: Login ''SQLDBATEST'' does not exist' WHERE UPPER (login_name) = 'SQLDBATEST';
 ELSE
-BEGIN TRY
-	EXEC ('ALTER LOGIN ' + @test_name + ' WITH PASSWORD = 0x<hash> HASHED, NAME = [SQLDBATest], DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF')
-	GRANT CONNECT SQL TO [SQLDBATest]
-	ALTER LOGIN [SQLDBATest] ENABLE
-	ALTER SERVER ROLE [sysadmin] ADD MEMBER [SQLDBATest]
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), [name], [create_date], [modify_date], 'CHANDGE PWD [SQLDBATest]', 'SUCCESS: ' + ERROR_MESSAGE() AS ErrorMessage
-	FROM master.sys.server_principals
-	WHERE UPPER (name) =  UPPER (@test_name); 
-END TRY  
+       BEGIN TRY
+             EXEC ('ALTER LOGIN ' + @test_name + ' WITH PASSWORD = 0x<hash> HASHED, NAME = [SQLDBATest], DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF')
+             GRANT CONNECT SQL TO [SQLDBATest]
+             ALTER LOGIN [SQLDBATest] ENABLE
+             ALTER SERVER ROLE [sysadmin] ADD MEMBER [SQLDBATest]
+             UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'SUCCESS: ' + ERROR_MESSAGE() WHERE UPPER (login_name) =  UPPER (@test_name); 
+       END TRY  
 
-BEGIN CATCH  
-	IF @test_name IS NULL SET @test_name = 'SQLDBATEST'
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), @test_name , [create_date], [modify_date], 'CHANDGE PWD [SQLDBATest]', 'ERROR: ' + ERROR_MESSAGE() AS ErrorMessage
-	FROM master.sys.server_principals
-	WHERE UPPER (name) =  UPPER (@test_name); 
-END CATCH;  
+       BEGIN CATCH  
+             UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'ERROR: ' + ERROR_MESSAGE() WHERE UPPER (login_name) =  UPPER (@test_name); 
+       END CATCH;  
 
 IF (SELECT [is_disabled] FROM master.sys.server_principals WHERE LOWER (name) = 'sa') = 1
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), 'sa', [create_date], [modify_date], 'DISABLE [sa]', 'SKIPPED: Login ''sa'' is disabled'
-	FROM master.sys.server_principals
-	WHERE LOWER (name) = 'sa'
+       UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'SKIPPED: Login ''sa'' is disabled' WHERE LOWER (login_name) = 'sa'
 ELSE
-BEGIN TRY
-ALTER LOGIN [sa] DISABLE
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), [name], [create_date], [modify_date], 'DISABLE [sa]', 'SUCCESS: ' + ERROR_MESSAGE() AS ErrorMessage
-	FROM master.sys.server_principals
-	WHERE LOWER (name) = 'sa'
-END TRY  
+       BEGIN TRY
+             ALTER LOGIN [sa] DISABLE
+             UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'SUCCESS: ' + ERROR_MESSAGE() WHERE LOWER (login_name) = 'sa'
+       END TRY  
 
-BEGIN CATCH  
-	INSERT INTO #ACC_PWD_CHANGE_HISTORY (date_time, server_name, collation, login_name, create_date, modify_date, action, ErrorMessage)
-	SELECT GETDATE (), SERVERPROPERTY('SERVERNAME'), SERVERPROPERTY('COLLATION'), [name], [create_date], [modify_date], 'DISABLE [sa]', 'ERROR' + ERROR_MESSAGE() AS ErrorMessage
-	FROM master.sys.server_principals
-	WHERE UPPER (name) =  UPPER ('sa'); 
-END CATCH;
+       BEGIN CATCH
+             UPDATE #ACC_PWD_CHANGE_HISTORY SET date_time = GETDATE (), ErrorMessage = 'ERROR' + ERROR_MESSAGE() WHERE LOWER (login_name) = 'sa'
+       END CATCH;
 
-SELECT * FROM #ACC_PWD_CHANGE_HISTORY
+UPDATE #ACC_PWD_CHANGE_HISTORY
+SET
+[server_name] = SERVERPROPERTY('SERVERNAME'),
+[collation] = SERVERPROPERTY('COLLATION')
+
+UPDATE tt
+SET
+tt.[login_name] = sp.[name],
+tt.[create_date] = sp.[create_date], 
+tt.[modify_date] = sp.[modify_date]
+FROM master.sys.server_principals AS sp
+JOIN #ACC_PWD_CHANGE_HISTORY AS tt ON sp.[name] = tt.[login_name]
+WHERE LOWER(tt.[login_name]) IN ('zabbix_mssql', 'zabbix_mssql_ts', 'sqldba', 'sqldbatest', 'sa')
+
+SELECT * FROM UPDATE #ACC_PWD_CHANGE_HISTORY
