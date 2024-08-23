76bfcb5bed914ebf14e3e4c2aa92b66006676d4b
dvpingvin <dvpingvin@gmail.com>
2024-08-23 12:21:16 +1000

    Update restore_log_query.sql

--- restore_log_query.sql
+++ restore_log_query.sql
@@ -1,9 +1,18 @@
+DECLARE @full_backup_n INT = 0 -- OFFSET: 0 - last full backup, 1 - previus full backup, etc ...
+DECLARE @full_backup_start_date DATETIME
+
+SELECT @full_backup_start_date = backup_start_date FROM msdb.dbo.backupset
+WHERE database_name = DB_NAME() AND recovery_model = 'FULL' AND is_copy_only = 0 AND type = 'D'
+ORDER BY backup_start_date DESC OFFSET @full_backup_n ROWS FETCH NEXT 1 ROWS ONLY;
+
+SELECT @full_backup_start_date AS last_full_backup_start_date
+
 SELECT
 s.server_name,
 s.database_name,
 --s.recovery_model,
-s.backup_start_date,
-s.backup_finish_date,
+s.backup_start_date AS log_backup_start_date,
+s.backup_finish_date AS log_backup_finish_date,
 'RESTORE LOG [' + DB_NAME() + '] FROM DISK = N''' + m.physical_device_name + ''' WITH NORECOVERY, STATS = 5' AS [restore_log_query],
 CAST(CAST(s.backup_size / 1000000 AS INT) AS VARCHAR(14)) + ' ' + 'MB' AS bkSize,
 CONVERT(CHAR(12), s.backup_finish_date - s.backup_start_date, 114) AS TimeTaken,
@@ -11,8 +20,11 @@ Is_copy_only,
 CAST(s.first_lsn AS VARCHAR(50)) AS first_lsn,
 CAST(s.last_lsn AS VARCHAR(50)) AS last_lsn,
 m.physical_device_name
+
 FROM msdb.dbo.backupset s
 INNER JOIN msdb.dbo.backupmediafamily m ON s.media_set_id = m.media_set_id
-WHERE s.database_name = DB_NAME() AND s.[type] = 'L' AND s.recovery_model = 'FULL' AND DATEDIFF(hh, s.backup_finish_date, GETDATE() ) < 3 * 24 -- for last 3 days
+WHERE s.database_name = DB_NAME() AND s.[type] = 'L' AND s.recovery_model = 'FULL' 
+AND s.backup_start_date > @full_backup_start_date
+--AND DATEDIFF(hh, s.backup_finish_date, GETDATE() ) < 3 * 24 -- for last 3 days
 ORDER BY backup_start_date ASC
 GO
